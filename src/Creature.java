c3158d8f95fcbbd55ef4b51b2f8a6ccc3235a06b
spyrosfok <it1491@uom.edu.gr>
2017-05-09 16:40:27 +0300

    First attribs on creature class

--- src/Creature.java
+++ src/Creature.java
@@ -1,9 +1,27 @@
-
+import java.util.ArrayList;
 public class Creature {
 	
-	String name;
-	int currentHP;
-	int maxHP;
+	private String name;
+	private int currentHP;
+	private int maxHP;
+	private String alignment;
+	private int size;
+	private String type;
+	private ArrayList<Integer> AbilityScores;
+	private ArrayList<Integer> AbilityModifiers;
+	private int ArmorClass;
+	private ArrayList<String> Vulnerabilities;
+	private ArrayList<String> Immunities;
+	private ArrayList<String> Resistances;
+	private boolean isSpellcaster;
+	private int SpellSaveDC;
+	private int SpellAttackModifier;
+	private String SpellcastingMod;
+	private int speed;
+	private ArrayList<String> ConditionImmunities;
+	private ArrayList<String> SavingThrowBonuses;
+	
+	
 	
 
 }
